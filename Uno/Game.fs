d22f74939e5aa629e0c7c806d8ecd1f7e0e1f438
Malte Finsterwalder <malte.finsterwalder@red6-es.de>
2017-10-23 10:58:18 +0200

    add comments

--- Uno/Game.fs
+++ Uno/Game.fs
@@ -50,10 +50,10 @@ type State =
     | Playing of Playing
 
 and Playing = {
-    LastCard: Card
-    NextPlayer: PlayerId
-    Direction: Direction
-    NumberOfPlayers: PlayerCount
+    LastCard: Card // The card that was last played and is at the top of the stack
+    NextPlayer: PlayerId // The PlayerId of the player that should play next
+    Direction: Direction // The current direction - not used yet
+    NumberOfPlayers: PlayerCount // Total number of players needed to determine next player
 }    
 
 type GameError = 
@@ -69,6 +69,8 @@ type Evolve = State -> Event -> State
 // Make the simplest implementation for the following signature
 // Command -> State -> Event list Result
 
+// Determine the next player. Currently very simple, only increment and wrap around.
+// Skip-Card not yet implemented
 let next (PlayerId id) (PlayerCount n) = PlayerId(id+1 % n)
 
 let decide : Decide = fun  command state -> 
@@ -87,12 +89,12 @@ let decide : Decide = fun  command state ->
     | Playing pData, PlayCard cData when pData.NextPlayer <> cData.Player ->
         Ok [WrongPlayer {Player = cData.Player}]
     | Playing pData, PlayCard cData ->
-        match pData.LastCard, cData.Card with
+        match pData.LastCard, cData.Card with // Match several possible correct moves, that all fire two Events: CardPlayed and PlayerChanged
         | Skip c1, Skip c2 -> Ok [ CardPlayed { Player = cData.Player; Card = cData.Card }; PlayerChanged { Player = next cData.Player pData.NumberOfPlayers} ]
         | Skip (sColor), Digit (digit, dColor) when dColor = sColor -> Ok [ CardPlayed { Player = cData.Player; Card = cData.Card }; PlayerChanged { Player = next cData.Player pData.NumberOfPlayers} ]
         | Digit (digit, dColor), Skip (sColor) when dColor = sColor -> Ok [ CardPlayed { Player = cData.Player; Card = cData.Card }; PlayerChanged { Player = next cData.Player pData.NumberOfPlayers} ]
         | Digit (firstDigit, firstColor), Digit (secondDigit, secondColor) when firstColor = secondColor || firstDigit = secondDigit -> Ok [ CardPlayed { Player = cData.Player; Card = cData.Card }; PlayerChanged { Player = next cData.Player pData.NumberOfPlayers} ]
-        | _ ->
+        | _ -> // Otherwise a wrong card was played
             Ok [WrongCardPlayed {
                 Player = cData.Player
                 Card = cData.Card
@@ -104,6 +106,11 @@ let decide : Decide = fun  command state ->
 // s -> Event [] -> sbyte
 
 //  (s + ([a;b;c] @ [e;f;g])) = (s + [a;b;c]) + [e; f; g]) 
+
+// Evolve should be kept very simple. There should be no business rules, that might change over time.
+// Otherwise it becomes problematic to replay the events after a change in the business rules.
+// To prevent this, important rules should be in the decide-function and stored in the Events.
+// Evolve only transfers the data from the Event to the State: NextPlayer in our case.
 let evolve : Evolve =
     fun state event -> 
         match state, event with
@@ -112,7 +119,7 @@ let evolve : Evolve =
             Direction = Right
             NextPlayer = PlayerId 0
             NumberOfPlayers = e.Players }
-        | Playing s, CardPlayed e -> Playing {
+        | Playing s, CardPlayed e -> Playing { 
             LastCard = e.Card 
             Direction = Right
             NextPlayer = s.NextPlayer
@@ -123,4 +130,3 @@ let evolve : Evolve =
             NextPlayer = p.Player
             NumberOfPlayers = s.NumberOfPlayers }
         | _ -> state
-
